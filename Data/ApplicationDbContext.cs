383ce0177aae488ab4908f62056326068d829718
liahra <christine@hellemyr.com>
2023-10-06 15:26:10 +0200

    Chr oppsett!

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Noested.Models.Movies;
+
+namespace Noested.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Noested.Models.Movies.Movie> Movie { get; set; } = default!;
+    }
+}
--- Noested/Models/RazorViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Noested.Models;
-
-public class RazorViewModel
-{
-    public string Content { get; set; }
-    public string AdditionalData { get; set; }
-}
