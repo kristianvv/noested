5404c57217ba567f9209c42d4334b922c61d5629
Issa Al-Musawi <issaal-musawi@Issas-MacBook-Air.local>
2023-10-06 21:20:02 +0200

    Innlogging program.cs

---
+++ Program 2.cs
@@ -0,0 +1,46 @@
+﻿using innlogging.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDatabase"));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "login",
+    pattern: "/login",
+    defaults: new { controller = "Login", action = "Index" }
+);
+
+app.MapControllerRoute(
+    name: "register",
+    pattern: "/register",
+    defaults: new { controller = "Register", action = "Index" }
+);
+
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
