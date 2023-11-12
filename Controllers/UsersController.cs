386de11519142efbca60770567a53520edb4dd58
liahra <christine@hellemyr.com>
2023-11-12 13:44:48 +0100

    lagt til User MVC, fikset styling på bl.a. søkebaren

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Noested.Models.ViewModels;
+
+namespace Noested.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users
+                .Select(u => new UserViewModel { UserId = u.Id, Email = u.Email })
+                .ToList();
+
+            var viewModel = new UserListViewModel
+            {
+                Users = users
+            };
+
+            return View(viewModel);
+        }
+    }
+}
