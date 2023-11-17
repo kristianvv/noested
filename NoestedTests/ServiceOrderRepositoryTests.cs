eb34e086a27b0ac164db3f7cb2db99359aa4519d
felixlhh <felixlh@uia.no>
2023-11-17 14:58:29 +0100

    passerende test

--- NoestedTests/ServiceOrderRepositoryTests.cs
+++ NoestedTests/ServiceOrderRepositoryTests.cs
@@ -1,11 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Noested.Data;
+using Noested.Data.Repositories;
+
 namespace NoestedTests
 {
-    public class UnitTest1
+    public class ServiceOrderRepositoryTests
     {
-        [Fact]
-        public void Test1()
+        private static ServiceOrderRepository Setup()
         {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
 
+            var context = new ApplicationDbContext(optionsBuilder.Options);
+
+            var repo = new ServiceOrderRepository(context, null);
+
+            return repo;
+        }
+
+        [Fact]
+        public async Task SearchTest()
+        {
+            // Arrange
+            var repository = Setup();
+            await DatabaseSeeder.SeedServiceOrders(repository);
+
+            // Act
+            var result = await repository.Search("John");
+
+            // Assert
+
+            Assert.Equal("John", result.FirstOrDefault().Customer.FirstName);
         }
     }
 }
