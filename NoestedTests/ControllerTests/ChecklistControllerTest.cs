fbb9297ab791f038edd75d67b3308e3e04f347d9
Frozeh <112623460+kristianvv@users.noreply.github.com>
2023-11-25 19:01:51 +0100

    Unit tester: UserControllerTest og ChecklistControllerTest

---
+++ NoestedTests/ControllerTests/ChecklistControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Noested.Controllers;
+using Noested.Data;
+using Noested.Models;
+
+namespace NoestedTests.ControllerTests
+{
+    public class ChecklistControllerTest
+    {
+        [Fact]
+        public async Task Can_Add_Checklist_To_Database()
+        {
+            // Arrange
+            var dbContext = await GetDbContext();
+            var customersController = new CustomersController(dbContext);
+
+            // Act
+            var customer = new Customer
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Street = "123 Main St",
+                PostalCode = "12345",
+                City = "City",
+                Email = "john.doe@example.com",
+                Phone = "555-1234"
+            };
+
+            var result = await customersController.Create(customer);
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal(nameof(CustomersController.Index), viewResult.ActionName);
+
+            Assert.Equal(1, dbContext.Customer.Count());
+            var addedCustomer = dbContext.Customer.FirstOrDefault();
+            Assert.NotNull(addedCustomer);
+            Assert.Equal("John", addedCustomer.FirstName);
+            
+            // Insert assertions based on the data model
+        }
+
+        private static async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            return databaseContext;
+        }
+    }
+}
---
+++ NoestedTests/ControllerTests/UserControllerTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Noested.Controllers;
+using Noested.Models.ViewModels;
+
+namespace NoestedTests.ControllerTests
+{
+    public class UsersControllerTest
+    {
+        [Fact]
+        public async Task Index_ReturnsViewWithUserList()
+        {
+            // Arrange
+            var userManagerMock = MockUserManager();
+            var roleManagerMock = MockRoleManager();
+
+            var controller = new UsersController(userManagerMock.Object, roleManagerMock.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<UserListViewModel>(viewResult.ViewData.Model);
+            Assert.NotNull(model.Users);
+            Assert.Equal(2, model.Users.Count()); // Adjust based on your expected user count
+        }
+
+        [Fact]
+        public async Task Edit_Get_ReturnsNotFound_WhenUserIdIsNull()
+        {
+            // Arrange
+            var userManagerMock = MockUserManager();
+            var roleManagerMock = MockRoleManager();
+
+            var controller = new UsersController(userManagerMock.Object, roleManagerMock.Object);
+
+            // Act
+            var result = await controller.Edit(userId: null);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        // Add more test methods for other actions as needed
+
+        private static Mock<UserManager<IdentityUser>> MockUserManager()
+        {
+            var users = new List<IdentityUser>
+            {
+                new IdentityUser { Id = "1", UserName = "user1", Email = "user1@example.com" },
+                new IdentityUser { Id = "2", UserName = "user2", Email = "user2@example.com" }
+            }.AsQueryable();
+
+            var store = new Mock<IUserStore<IdentityUser>>();
+            store.Setup(x => x.FindByIdAsync(It.IsAny<string>(), It.IsAny<System.Threading.CancellationToken>()))
+                .ReturnsAsync((string userId, System.Threading.CancellationToken cancellationToken) =>
+                {
+                    return users.FirstOrDefault(u => u.Id == userId);
+                });
+
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            userManagerMock.Setup(x => x.Users).Returns(users);
+
+            return userManagerMock;
+        }
+
+        private static Mock<RoleManager<IdentityRole>> MockRoleManager()
+        {
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole { Id = "1", Name = "Admin" },
+                new IdentityRole { Id = "2", Name = "User" }
+            }.AsQueryable();
+
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(
+                new Mock<IRoleStore<IdentityRole>>().Object, null, null, null, null);
+
+            roleManagerMock.Setup(x => x.Roles).Returns(roles);
+
+            return roleManagerMock;
+        }
+    }
+}
